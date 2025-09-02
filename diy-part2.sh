56533d430f5b7c5b9b00f7cbc570a4f4b1a0a994
sfwtw <wtw@cr.cx>
2025-09-02 18:27:26 +0800

    Init

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,4 +29,16 @@ function git_sparse_clone() {
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # TTYD 免登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 修改99_custom文件中的OPENWRT_RELEASE信息
+UCI_DEFAULTS_FILE="target/linux/mediatek/filogic/base-files/etc/uci-defaults/99_custom"
+if [ -f "$UCI_DEFAULTS_FILE" ]; then
+    sed -i '/sed -i.*OPENWRT_RELEASE.*\/usr\/lib\/os-release/d' "$UCI_DEFAULTS_FILE"
+    sed -i '/echo.*OPENWRT_RELEASE.*\/usr\/lib\/os-release/d' "$UCI_DEFAULTS_FILE"
+    # 在exit 0上面插入新的内容
+    VERSION_DATE="R$(date +%y.%-m.%-d)"
+    sed -i '/^exit 0$/i\
+sed -i '"'"'\/OPENWRT_RELEASE\/d'"'"' \/usr\/lib\/os-release\
+echo "OPENWRT_RELEASE='"'"'ImmortalWrt (with 237'"'"'s Full HNAT) \/ '"$VERSION_DATE"''"'"'" >> \/usr\/lib\/os-release' "$UCI_DEFAULTS_FILE"
+fi
